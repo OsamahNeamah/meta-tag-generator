f545f572bc2bb005fb01ead99d2be1b365ece036
Osamah <oneamah@gmail.com>
2023-07-20 23:27:06 -0400

    Marmil_Meta_Tag_Generator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';  
+import "./styles/styles.css";
+import Card from "react-bootstrap/Card";
+import React, { useState, useEffect } from "react";
+import GeneratedMetaTag from "./components/GenerateMetaTag";
+import { generateMetaTags } from "./Generator/Generator";
+import { MetaTagForm } from "./components/MetaTagForm";
+const App = () => {
+  const [metaTags, setMetaTags] = useState("");
+  const [metaTagsCollections, setMetaTagsCollections] = useState({
+    title: "",
+    description: "",
+    keywords: "",
+    author: "",
+    url: "",
+    imageUrl: "",
+  });
 
-function App() {
+  useEffect(() => {
+    setMetaTagsCollections(metaTagsCollections)
+    let generatedMetaTags = generateMetaTags(metaTagsCollections);
+    setMetaTags(generatedMetaTags);
+  }, [metaTagsCollections]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App Container">
+      <title>Meta Tag Generator</title>
+      <Card style={{ width: "100%" }}>
+        <Card.Body>
+          <h1 className="meta-tag-title">Meta Tag Generator</h1>
+          <MetaTagForm
+            metaTagsCollections={metaTagsCollections}
+            setMetaTagsCollections={setMetaTagsCollections}
+            setMetaTags={setMetaTags}
+            metaTags={metaTags}
+          />
+          <GeneratedMetaTag metaTags={metaTags}/>
+        </Card.Body>
+      </Card>
     </div>
   );
-}
-
+};
 export default App;
